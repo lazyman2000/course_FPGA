ab7b0a7fda1242f355680982c6d1039e98eac2c0
lazyman2000 <50194364+lazyman2000@users.noreply.github.com>
2024-12-11 18:59:26 +0300

    Create uart_if.sv
    Заголовочный файл UART

---
+++ UART/rtl/if/uart_if.sv
@@ -0,0 +1,20 @@
+interface uart_if
+  #(parameter
+    DATA_WIDTH = 8);
+
+   logic                  sig; // сигнал передачи/приема.
+   logic [DATA_WIDTH-1:0] data; // передаваемые или принимаемые данные.
+   logic                  valid; // флаг, сигнализирующий о готовности данных для передачи.
+   logic                  ready; // флаг готовности устройства к передаче или приему.
+
+   modport tx(output sig,
+              input  data,
+              input  valid,
+              output ready);
+
+   modport rx(input  sig,
+              output data,
+              output valid,
+              input  ready);
+
+endinterface
