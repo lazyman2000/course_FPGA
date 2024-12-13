bd8f434ac83491fe966ac67ceeaca2e0d9a9e667
KristinaChriss <159703826+KristinaChriss@users.noreply.github.com>
2024-12-14 02:20:33 +0300

    Added comb logic for states

--- HCSR04/HCSR04.sv
+++ HCSR04/HCSR04.sv
@@ -20,38 +20,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module HCSR04(input logic clk, rst, start, echo, 
-output logic trig, val, 
-output logic [11:0] distance); // in mm
+module HCSR04(input logic clk, rst, start, echo,
+output logic trig, val,                          
+output logic [11:0] distance);                   
 logic [2:0] state;
 logic [21:0] cnt_e;
 logic [9:0] cnt_t; // cnt = 10ns
 
- enum logic [2:0] {RESET = 3'd0, SIG = 3'd1, START = 3'd1, TRIGGER = 3'd2, WAITER = 3'd3, ECHO = 3'd4, DONE = 3'd5} STATE;
+enum logic [2:0] {RESET = 3'd0, SIG = 3'd1, START = 3'd2, TRIGGER = 3'd3, WAITER = 3'd4, ECHO = 3'd5, DONE = 3'd6} STATE;
 
- always_ff @(posedge clk or negedge rst)
+always_ff @(posedge clk or negedge rst)
 begin
- if (!rst)
+if (!rst)
 state <= RESET;
 else
- state <= SIG;
+state <= SIG;
 case (state)
 RESET: 
 begin
-val <=0;
-trig <=0;
-cnt_t <=0;
-cnt_e <=0;
- distance <= 0;
 state <= SIG;
 end
- SIG: 
+SIG: 
 begin
-val <=0;
-trig <=0;
-cnt_t <=0;
-cnt_e <=0;
-distance <= 0;
  if (start)
  state <= START;
  else
@@ -100,20 +90,15 @@ DONE:
 begin
  if (cnt_e == 379999)  // 38 ms 
   begin
-  val <=0;
-  distance <= 0;
   state <= RESET;
   end
  else
  begin
   if (cnt_e > 0)
    begin
-   distance <= cnt_e*17/10000; // 340 m/s = 34*10^-5 ss/ns; N*cnt = N*10 ns; takes half => 17  
-   val<=1;
    state <= RESET;
    end
   else
-  val <=0;
   state <= RESET;
  end
 end
@@ -125,6 +110,46 @@ end
   cnt_t <=0; 
   state <= RESET;
   end
+endcase
+end
+
+always_comb
+begin
+case(state)
+RESET: 
+begin
+val <=0;
+trig <=0;
+cnt_t <=0;
+cnt_e <=0;
+distance <= 0;
+end
+SIG: 
+begin
+val <=0;
+trig <=0;
+cnt_t <=0;
+cnt_e <=0;
+distance <= 0;
+end
+DONE: 
+begin
+ if (cnt_e == 379999)  // 38 ms 
+  begin
+  val <=0;
+  distance <= 0;
+  end
+ else
+ begin
+  if (cnt_e > 0)
+   begin
+   distance <= cnt_e*17/10000; // 340 m/s = 34*10^-5 ss/ns; N*cnt = N*10 ns; takes half => 17  
+   val<=1;
+   end
+  else
+  val <=0;
+ end
+end
 endcase
 end
 endmodule
