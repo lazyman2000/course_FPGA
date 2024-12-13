4d88f32d3d62f800c3f200463bb9cde06dc7ca89
lazyman2000 <50194364+lazyman2000@users.noreply.github.com>
2024-12-13 18:51:47 +0300

    Update uart_rx.sv
    Обновленный код без интерфейсов

--- UART/rtl/uart_rx.sv
+++ UART/rtl/uart_rx.sv
@@ -1,5 +1,4 @@
 `include "if/uart_if.sv"
-
 module uart_rx
   #(parameter
     DATA_WIDTH = 8,
@@ -7,37 +6,35 @@ module uart_rx
     CLK_FREQ   = 100_000_000,
 
     localparam
-    LB_DATA_WIDTH    = $clog2(DATA_WIDTH), // Количество бит, необходимое для адресации всех бит данных
-    PULSE_WIDTH      = CLK_FREQ / BAUD_RATE, // Количество тактовых импульсов для одной длительности бита
+    LB_DATA_WIDTH    = $clog2(DATA_WIDTH),
+    PULSE_WIDTH      = CLK_FREQ / BAUD_RATE,
     LB_PULSE_WIDTH   = $clog2(PULSE_WIDTH),
     HALF_PULSE_WIDTH = PULSE_WIDTH / 2)
-   (uart_if.rx  rxif,
+   (
+    input logic rx_sig,
     input logic clk,
     input logic rstn,
-    input logic sensor_ready,         // Сигнал готовности датчика
-    output logic [DATA_WIDTH-1:0] sensor_data, // Данные, отправляемые в датчик
-    output logic sensor_valid         // Сигнал валидации данных для датчика
+    input logic sensor_ready,
+    output logic [DATA_WIDTH-1:0] sensor_data,
+    output logic sensor_valid
    );
 
-   //----------------------------------------------------------------
-   // description about receive UART signal
    typedef enum logic [1:0] {STT_DATA,
                              STT_STOP,
                              STT_WAIT} statetype;
 
-   statetype                 state;
+   statetype state;
 
-   logic [DATA_WIDTH-1:0]    data_tmp_r; // Регистр для временного хранения принимаемых данных
-   logic [LB_DATA_WIDTH:0]   data_cnt;   // Счетчик битов данных
-   logic [LB_PULSE_WIDTH:0]  clk_cnt;    // Счетчик тактов для синхронизации с длительностью бита
-   logic                     rx_done;   // Флаг, указывающий, что все данные успешно приняты
+   logic [DATA_WIDTH-1:0] data_tmp_r;
+   logic [LB_DATA_WIDTH:0] data_cnt;
+   logic [LB_PULSE_WIDTH:0] clk_cnt;
+   logic rx_done;
 
-   // Регистр для хранения данных перед отправкой в датчик
-   logic [DATA_WIDTH-1:0]    buffer_r;
-   logic                     buffer_valid;
+   logic [DATA_WIDTH-1:0] buffer_r;
+   logic buffer_valid;
 
-   always_ff @(posedge clk) begin
-      if(!rstn) begin
+   always_ff @(posedge clk or negedge rstn) begin
+      if (!rstn) begin
          state        <= STT_WAIT;
          data_tmp_r   <= 0;
          data_cnt     <= 0;
@@ -46,43 +43,34 @@ module uart_rx
          buffer_valid <= 0;
          sensor_valid <= 0;
          sensor_data  <= 0;
-      end
-      else begin
-
-         //---------------------------------------------------------------------- 
-         // FSM for UART reception
-         case(state)
-
+      end else begin
+         case (state)
            STT_DATA: begin
-              if(0 < clk_cnt) begin
+              if (0 < clk_cnt) begin
                  clk_cnt <= clk_cnt - 1;
-              end
-              else begin
-                 data_tmp_r <= {rxif.sig, data_tmp_r[DATA_WIDTH-1:1]}; // Сдвиговый регистр
+              end else begin
+                 data_tmp_r <= {rx_sig, data_tmp_r[DATA_WIDTH-1:1]};
                  clk_cnt    <= PULSE_WIDTH;
-
-                 if(data_cnt == DATA_WIDTH - 1) begin // Если все биты данных приняты
+                 if (data_cnt == DATA_WIDTH - 1) begin
                     state <= STT_STOP;
-                 end
-                 else begin
+                 end else begin
                     data_cnt <= data_cnt + 1;
                  end
               end
            end
 
            STT_STOP: begin
-              if(0 < clk_cnt) begin
+              if (0 < clk_cnt) begin
                  clk_cnt <= clk_cnt - 1;
-              end
-              else if(rxif.sig) begin // Проверяется стоповый бит
-                 state <= STT_WAIT;
-                 buffer_r     <= data_tmp_r; // Сохранение данных в буфер
-                 buffer_valid <= 1;          // Установка флага наличия данных
+              end else if (rx_sig) begin
+                 state        <= STT_WAIT;
+                 buffer_r     <= data_tmp_r;
+                 buffer_valid <= 1;
               end
            end
 
            STT_WAIT: begin
-              if(rxif.sig == 0) begin // Ожидание стартового бита
+              if (rx_sig == 0) begin
                  clk_cnt  <= PULSE_WIDTH + HALF_PULSE_WIDTH;
                  data_cnt <= 0;
                  state    <= STT_DATA;
@@ -92,14 +80,12 @@ module uart_rx
            default: state <= STT_WAIT;
          endcase
 
-         // Проверка готовности датчика и отправка данных
-         if(buffer_valid && sensor_ready) begin
-            sensor_data  <= buffer_r; // Передача данных в датчик
-            sensor_valid <= 1;       // Уведомление о готовности данных
-            buffer_valid <= 0;       // Очищение буфера
-         end
-         else begin
-            sensor_valid <= 0; // Отключение валидации, если датчик не готов
+         if (buffer_valid && sensor_ready) begin
+            sensor_data  <= buffer_r;
+            sensor_valid <= 1;
+            buffer_valid <= 0;
+         end else begin
+            sensor_valid <= 0;
          end
       end
    end
