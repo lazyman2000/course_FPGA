4a119129e1ab67075ec2648abe5f2b1405f5a9d8
Roman <my@email.com>
2024-12-14 20:51:37 +0300

    Ver 6 (Final)

--- CROSSBAR/BCD_SV_ff.sv
+++ CROSSBAR/BCD_SV_ff.sv
@@ -4,9 +4,9 @@ module BCD_SV_ff(
     input logic [13:0] bin_in,
     input logic clk,
     input logic rst,
-    output logic [31:0] ascii_out,
-    input logic cross_ready,
-    output logic bcd_ready
+    output logic [31:0] ascii_out, 
+    input logic cross_ready, 
+    output logic bcd_ready 
 );
 
     logic [13:0] bin;
@@ -23,6 +23,7 @@ module BCD_SV_ff(
     end
     
     always_comb begin
+    if (cross_ready) begin //if crossbar recieved data from sensor
         case(state)
             START: 
                 next_state = SHIFT;
@@ -43,7 +44,7 @@ module BCD_SV_ff(
                 next_state = RESET;
         endcase
     end
-    
+    end
     always_ff @(posedge clk or negedge rst) begin
         if (!rst) begin
             bin <= 'd0;
@@ -51,24 +52,30 @@ module BCD_SV_ff(
             i <= 'd0;
             bcd_ready <= 1'b0;
             ascii_out <= 32'b0;
-        end else if (cross_ready) begin
+        end else if (cross_ready) begin 
             case(state)
                 START: begin
                     bin <= bin_in;
                     bcd <= 'd0;
                     i <= 4'd0;
                     bcd_ready <= 1'b0;
+                    $display("inside START");
+                    $display("START bin: %d", bin);
                 end
                 SHIFT: begin
                     bin <= {bin[12:0], 1'd0};
                     bcd <= {bcd[14:0], bin[13]};
                     i <= i + 4'd1;
+                    $display("inside SHIFT");
+                    $display("SHifted bin: %d", bin);
+                    $display("bcd is: %d",bcd);
                 end
                 ADD: begin
                     if (bcd[3:0] > 'd4) bcd[3:0] <= bcd[3:0] + 4'd3;
                     if (bcd[7:4] > 'd4) bcd[7:4] <= bcd[7:4] + 4'd3;
                     if (bcd[11:8] > 'd4) bcd[11:8] <= bcd[11:8] + 4'd3;
                     if (bcd[15:12] > 'd4) bcd[15:12] <= bcd[15:12] + 4'd3;
+                    $display("inside ADD");
                 end
                 DONE: begin
                     ascii_out <= {
@@ -77,19 +84,21 @@ module BCD_SV_ff(
                         (bcd[7:4] + 8'd48),
                         (bcd[3:0] + 8'd48)};
                     i <= 'd0;
+                    $display("inside DONE");
                 end
                 FLAG: begin //just to guarantee that flag is set after the output assignment
                     bcd_ready <= 1'b1;
+                    $display("inside FLAG");
                 end
                 RESET: begin
                     bin <= 'd0;
                     bcd <= 'd0;
                     i <= 'd0;
                     bcd_ready <= 1'b0;
+                    $display("inside RESET");
                 end
             endcase
         end
         //$display("bin_in: %b, bcd: %b, ascii_out: %b",bin_in,bcd,ascii_out);
     end
 endmodule
-
--- CROSSBAR/Crossbar_pipeline.sv
+++ CROSSBAR/Crossbar_pipeline.sv
@@ -23,16 +23,23 @@ module Crossbar_pipeline(
     input logic dht11_data_available, //is data from DHT11 available? (should be renamed)
     output logic hc_sr04_start, //signal for initializing HCSR04 measurements (should be renamed)
     input logic [15:0] hc_sr04_data, //received data from HCSR04 (should be renamed)
-    input logic hc_sr04_data_available //is data from DHT11 available? (should be renamed)
+    input logic hc_sr04_data_available, //is data from DHT11 available? (should be renamed)
+    input logic valid_command, //signal from UART, if command is valid
+    output logic ready_to_act, //1 by default. 0 when recieved valid command and until data from sensors sent to UART
+    output logic data_transaction_ready //pulse that shows data is ready to be sent
     );
     
     //numerable variable for device state
-    typedef enum logic [2:0] {
-        IDLE = 3'b000, //wait for command
-        START_TEMP_N_MOIST = 3'b001,
-        START_DIST = 3'b010,
-        READ_TEMP_N_MOIST = 3'b011, //type 8'h54 ("T") for temperature and moisture measurement
-        READ_DIST = 3'b100 //type 8'h44 ("D") for distance measurement
+    typedef enum logic [3:0] {
+        IDLE = 4'b000, //wait for command
+        START_TEMP_N_MOIST = 4'b001, //type 8'h54 ("T") for temperature and moisture measurement
+        START_DIST = 4'b010, //type 8'h44 ("D") for distance measurement
+        READ_TEMP_N_MOIST = 4'b011, 
+        READ_DIST = 4'b100, 
+        ASCII_TEMP_N_MOIST = 4'b101,
+        ASCII_DIST = 4'b110,
+        SEND_TEMP_N_MOIST = 4'b111,
+        SEND_DIST = 4'b1000
     } device_state;
     
     device_state command_decode;
@@ -54,8 +61,10 @@ module Crossbar_pipeline(
     always_ff @(posedge clk or negedge rst) begin
         if (!rst) begin
             uart_command_fetch <= 8'b0;
-        end else begin
+            ready_to_act <= 1'b1;
+        end else if (ready_to_act && valid_command) begin
             uart_command_fetch <= uart_rx; //write the data from UART
+            ready_to_act <= 1'b0; //when the valid_command is up module doesn't read any commands until the cycle is done
         end
     end
     
@@ -71,19 +80,21 @@ module Crossbar_pipeline(
             bcd_temp <= 1'b0;
             bcd_moist <= 1'b0;
             bcd_dist <= 1'b0;
+            data_transaction_ready <= 1'b0;
         end else begin
            case (command_decode)
-                IDLE: begin
+                IDLE: begin //default state, waiting for the command
                     dht11_start <= 1'b0;
                     hc_sr04_start <= 1'b0;
                     execute_data <= 16'b0;
                     bcd_temp <= 1'b0;
                     bcd_moist <= 1'b0;
                     bcd_dist <= 1'b0;
+                    data_transaction_ready <= 1'b0;
                     
-                    if (uart_command_fetch == 8'h54) begin
+                    if (!ready_to_act && uart_command_fetch == 8'h54) begin
                         command_decode <= START_TEMP_N_MOIST;
-                    end else if (uart_command_fetch == 8'h44) begin
+                    end else if (!ready_to_act && uart_command_fetch == 8'h44) begin
                         command_decode <= START_DIST;
                     end else begin
                         command_decode <= IDLE;
@@ -98,72 +109,94 @@ module Crossbar_pipeline(
                     command_decode <= READ_DIST;
                 end
                 READ_TEMP_N_MOIST: begin
-                    dht11_start <= 1'b0; //send the measurement initializing bit
+                    dht11_start <= 1'b0; //the initializing bit should be set only for 1 clk period
                     
                     if (dht11_data_available) begin //if there is any data in sensor buffer, read it
                         execute_data <= dht11_data; //place recieved data from DHT11 into buffer
-                        bcd_temp <= 1'b1;
-                        bcd_moist <= 1'b1;
+                        bcd_temp <= 1'b1; //flag to start BCD-ASCII transformation of the temperature data
+                        bcd_moist <= 1'b1; //flag to start BCD-ASCII transformation of the moisture data
+                        command_decode <= ASCII_TEMP_N_MOIST;
                     end
-                    
-                    if (ready_temp && ready_moist) begin
-                        if (!sending && uart_ready) begin
-                            sending <= 1'b1;
-                            counter <= 4'b0;
-                        end else if (execute_data !== 16'b0 && uart_ready) begin
-                            case (counter)
-                                4'd0: uart_tx <= ascii_data_temp[23:16]; 
-                                4'd1: uart_tx <= ascii_data_temp[15:8];
-                                4'd2: uart_tx <= ascii_data_temp[7:0];
-                                4'd3: uart_tx <= 8'h0D;
-                                4'd4: uart_tx <= ascii_data_moist[23:16];
-                                4'd5: uart_tx <= ascii_data_moist[15:8];
-                                4'd6: uart_tx <= ascii_data_moist[7:0];
-                                4'd7: uart_tx <= 8'h0D;
-                                default: sending <= 1'b0;
-                            endcase
-                            //$display("ascii_data_temp1: %b",ascii_data_temp[23:16]);
-                            //$display("ascii_data_temp2: %b",ascii_data_temp[15:8]);
-                            //$display("ascii_data_temp3: %b",ascii_data_temp[7:0]);
-                            //$display("ascii_data_moist1: %b",ascii_data_moist[23:16]);
-                            //$display("ascii_data_moist2: %b",ascii_data_moist[15:8]);
-                            //$display("ascii_data_moist3: %b",ascii_data_moist[7:0]);
-                            if (counter < 4'd7) begin
-                                counter <= counter + 1;
-                            end else begin
-                                sending <= 1'b0;
-                                command_decode <= IDLE;
-                            end
+                end
+                ASCII_TEMP_N_MOIST: begin
+                    if (ready_temp && ready_moist) begin //if the BCD-ASCII done it's job
+                        data_transaction_ready <= 1'b1;
+                        bcd_temp <= 1'b0; //flag to stop BCD-ASCII for temperature
+                        bcd_moist <= 1'b0; //flag to stop BCD-ASCII for moisture
+                        command_decode <= SEND_TEMP_N_MOIST;
+                    end
+                end
+                SEND_TEMP_N_MOIST: begin
+                    if (!sending && uart_ready) begin //if UART is ready to read and not sending
+                            sending <= 1'b1; //start sending
+                            counter <= 4'b0; //set counter constant to zero
+                    end else if (execute_data !== 16'b0 && uart_ready) begin //if there non-zero data in buffer and UART is ready to read
+                        case (counter) //send bytes
+                            4'd0: uart_tx <= ascii_data_temp[23:16]; 
+                            4'd1: uart_tx <= ascii_data_temp[15:8];
+                            4'd2: uart_tx <= ascii_data_temp[7:0];
+                            4'd3: uart_tx <= 8'h0D;
+                            4'd4: uart_tx <= ascii_data_moist[23:16];
+                            4'd5: uart_tx <= ascii_data_moist[15:8];
+                            4'd6: uart_tx <= ascii_data_moist[7:0];
+                            4'd7: uart_tx <= 8'h0D;
+                            default: sending <= 1'b0;
+                        endcase
+                        //$display("ascii_data_temp1: %b",ascii_data_temp[23:16]);
+                        //$display("ascii_data_temp2: %b",ascii_data_temp[15:8]);
+                        //$display("ascii_data_temp3: %b",ascii_data_temp[7:0]);
+                        //$display("ascii_data_moist1: %b",ascii_data_moist[23:16]);
+                        //$display("ascii_data_moist2: %b",ascii_data_moist[15:8]);
+                        //$display("ascii_data_moist3: %b",ascii_data_moist[7:0]);
+                        if (counter < 4'd7) begin
+                            counter <= counter + 1;
+                        end else begin
+                            sending <= 1'b0;
+                            ready_to_act <= 1'b1;
+                            data_transaction_ready <= 1'b0;
+                            command_decode <= IDLE;
                         end
                     end
                 end
+                    
                 READ_DIST: begin
                     hc_sr04_start <= 1'b0;
                     if (hc_sr04_data_available) begin
                         execute_data <= hc_sr04_data; //place recieved data from DHT11 into buffer
                         bcd_dist <= 1'b1;
+                        command_decode <= ASCII_DIST;
                     end
-                    
-                    if (ready_dist) begin             
-                        if (!sending && uart_ready) begin
-                            sending <= 1'b1;
-                            counter <= 4'b0;
-                        end else if (execute_data !== 16'b0 && uart_ready) begin      
-                            case (counter)
-                                4'd0: uart_tx <= ascii_data_dist[31:24];  
-                                4'd1: uart_tx <= ascii_data_dist[23:16];
-                                4'd2: uart_tx <= ascii_data_dist[15:8];
-                                4'd3: uart_tx <= ascii_data_dist[7:0];
-                                4'd4: uart_tx <= 8'h0D;
-                                default: sending <= 1'b0;
-                            endcase
-
-                            if (counter < 4'd4) begin
-                                counter <= counter + 1;
-                            end else begin
-                                sending <= 1'b0;
-                                command_decode <= IDLE;
-                            end
+                end
+                ASCII_DIST: begin
+                    if (ready_dist) begin
+                        data_transaction_ready <= 1'b1;
+                        bcd_dist <= 1'b0;
+                        command_decode <= SEND_DIST;
+                    end
+                end
+                SEND_DIST: begin
+                    if (!sending && uart_ready) begin
+                        sending <= 1'b1;
+                        counter <= 4'b0;
+                    end else if (execute_data !== 16'b0 && uart_ready) begin      
+                        case (counter)
+                            4'd0: uart_tx <= ascii_data_dist[31:24];  
+                            4'd1: uart_tx <= ascii_data_dist[23:16];
+                            4'd2: uart_tx <= ascii_data_dist[15:8];
+                            4'd3: uart_tx <= ascii_data_dist[7:0];
+                            4'd4: uart_tx <= 8'h0D;
+                            default: sending <= 1'b0;
+                        endcase
+                        //$display("ascii_data_dist1: %b",ascii_data_moist[23:16]);
+                        //$display("ascii_data_dist2: %b",ascii_data_moist[15:8]);
+                        //$display("ascii_data_dist3: %b",ascii_data_moist[7:0]);
+                        if (counter < 4'd4) begin
+                            counter <= counter + 1;
+                        end else begin
+                            sending <= 1'b0;
+                            ready_to_act <= 1'b1;
+                            data_transaction_ready <= 1'b0;
+                            command_decode <= IDLE;
                         end
                     end
                 end
@@ -175,6 +208,7 @@ module Crossbar_pipeline(
         end
     end
     
+    //BCD-ASCII instances
     BCD_SV_ff DHT11_TEMP(.bin_in({6'b0, execute_data[15:8]}), .clk(clk), .rst(rst), .ascii_out(ascii_data_temp), .cross_ready(bcd_temp), .bcd_ready(ready_temp));
     BCD_SV_ff DHT11_MOIST(.bin_in({6'b0, execute_data[7:0]}), .clk(clk), .rst(rst), .ascii_out(ascii_data_moist), .cross_ready(bcd_moist), .bcd_ready(ready_moist));
     BCD_SV_ff HCSR4(.bin_in(execute_data[13:0]), .clk(clk), .rst(rst), .ascii_out(ascii_data_dist), .cross_ready(bcd_dist), .bcd_ready(ready_dist));
