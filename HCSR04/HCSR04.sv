c2dd9b9cea48bc735ef1cc78b2eff67ebcf77233
KristinaChriss <159703826+KristinaChriss@users.noreply.github.com>
2024-12-24 08:07:38 +0300

    ver 5

--- HCSR04/HCSR04.sv
+++ HCSR04/HCSR04.sv
@@ -20,21 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module HCSR04(input logic clk, rst, start, echo,   // clk -10ns, start - from crossbar, echo - from sensor
+module HCSR04(input logic clk, rst, start, echo,   // clk - 20ns, start - from crossbar, echo - from sensor
 output logic trig, val,                            // trig - to sensor, val - to crossbar (validation)
 output logic [11:0] distance);                     // data for crossbar im mm
 logic [2:0] state;
-logic [21:0] cnt_e;                                // time counter of echo
-logic [9:0] cnt_t;                                 // time counter of trig 
-logic [27:0] dist_m;                               // data*10000 for divition
+logic [27:0] cnt_e;                                // time counter of echo
+logic [11:0] cnt_t;                                 // time counter of trig 
+logic [31:0] dist_m;                               // data*10000 for divition
 
 enum logic [2:0] {RESET = 3'd0, SIG = 3'd1, START = 3'd2, TRIGGER = 3'd3, WAITER = 3'd4, ECHO = 3'd5, DONE = 3'd6} STATE;
 
 always_ff @(posedge clk or negedge rst)
 begin
-if (!rst)
+if (!rst) begin
 state <= RESET;
-else
+trig <=0;
+cnt_t <=0;
+cnt_e <=0;
+end
+else begin
 state <= SIG;
 case (state)
 RESET:                                                    
@@ -55,15 +59,14 @@ state <= TRIGGER;
 end
 TRIGGER: 
 begin
-if (cnt_t == 999)       // waitin 10 us (1000*10ns); end of trig ignal
+if (cnt_t == 499)       // waitin 10 us (500*20ns); end of trig ignal
   begin
   trig <=0;         
-  cnt_t <= 0;
   state <= WAITER;
   end
  else
   begin
-  cnt_t <= cnt_t +1;  // count while cnt_t < 999
+  cnt_t <= cnt_t +1;  // count while cnt_t < 499
   state <= TRIGGER;  
   end 
 end
@@ -89,7 +92,7 @@ begin
 end
 DONE: 
 begin
- if (cnt_e == 379999)  // 38 ms If the waiting time is longer than 38 ms, the sensor has not found an object 
+ if (cnt_e == 1899999)  // 38 ms If the waiting time is longer than 38 ms, the sensor has not found an object 
   begin
   state <= RESET;
   end
@@ -97,7 +100,10 @@ begin
  begin
   if (cnt_e > 0)     // checked that echo is not null
    begin
-   state <= RESET;
+   state <= SIG;
+   trig <=0;
+   cnt_e <=0; 
+   cnt_t <=0;
    end
   else
   state <= RESET;
@@ -105,52 +111,27 @@ begin
 end
  default:
   begin
-  val <=0;
-  trig <=0;
-  cnt_e <=0; 
-  cnt_t <=0; 
   state <= RESET;
   end
 endcase
 end
+end
 
 always_comb
 begin
+val = 0;
+distance = 0;
+dist_m = 0;
 case(state)
-RESET:             // all variable in zero
-begin
-val <=0;
-trig <=0;
-cnt_t <=0;
-cnt_e <=0;
-distance <= 0;
-end
-SIG:               // same 
-begin
-val <=0;
-trig <=0;
-cnt_t <=0;
-cnt_e <=0;
-distance <= 0;
-end
 DONE: 
 begin
- if (cnt_e == 379999)
-  begin
-  val <=0;
-  distance <= 0;
-  end
- else
- begin
-  if (cnt_e > 0)
+  if ((cnt_e > 0) && (cnt_e != 1899999))
    begin
-   dist_m <= cnt_e*17;           // 340 m/s = 34*10^-5 ss/ns; N*cnt = N*10 ns; takes half => 17; 10^-5*10 => 10^-4            
-   distance <= (dist_m >>> 14) + (dist_m >>> 15) + (dist_m >>> 17) + (dist_m >>> 21) + (dist_m >>> 22) + (dist_m >>> 24)+ (dist_m >>> 25);  
-   val<=1;                     // 10^-4 = 0.00000000000001101000110110111 (shifi left by degrees corresponding to 1)
+   dist_m = cnt_e*34;           // 340 m/s = 34*10^-5 ss/ns; N*cnt = N*20 ns; takes half => 17*2; 10^-5*10 => 10^-4            
+   distance = (dist_m >>> 14) + (dist_m >>> 15) + (dist_m >>> 17) + (dist_m >>> 21) + (dist_m >>> 22) + (dist_m >>> 24)+ (dist_m >>> 25)
+   +(dist_m >>> 27)+(dist_m >>> 28)+(dist_m >>> 29);  
+   val = 1;                     // 10^-4 = 0.00000000000001101000110110111 (shifi left by degrees corresponding to 1)
    end                         // calculating the distance and setting the validity flag
-  else
-  val <=0;
- end
 end
 endcase
 end
