9bc07d23017eaa6dd0612b3f7a9d93f6f0ea3b62
katessii <159717141+katessii@users.noreply.github.com>
2024-12-26 14:50:08 +0300

    Update TB

--- DHT11/DHT11_TB.sv
+++ DHT11/DHT11_TB.sv
@@ -4,61 +4,85 @@
 
 module DHT11_TB;
     // Параметры
-    parameter CLK_PERIOD = 10; // Период тактового сигнала (100 МГц)
+    parameter CLK_PERIOD = 10; // Период тактового сигнала (50 МГц)
+    parameter DATA_WIDTH = 8;
+    parameter BAUD_RATE  = 115200;
+
 
     // Тестовые сигналы
     logic clk;
-    logic rst_n;
-    logic uart_rx; // сигнал запроса от Ромы
-    logic [15:0] uart_tx; // данные посылаемые Роме (сначала T, потом влажность)
-    logic ready; // сигнал готовности, когда uart_tx сформирован и отправленб ready=1; иначе ready=0
-    logic dht11_data_output; // когда 1 это выход (для датчика), когда 0 - вход для датчика
-    logic dht11_data_internal; // когда dht11_data_output=1, можно отправитб 1/0 микроконтроллеру
-    reg [39:0] data_sequence; // просто искуственный пример 40 бит данных, посылаемых датчиком
-    tri dht11_data; // однонаправленная шина
+logic rst_n;
+//logic uart_rx_i;
+//logic uart_tx_o;
+tri   dht11_data;
+//logic       echo;
+//logic       trig;
+logic dht11_data_output; // РєРѕРіРґР° 1 СЌС‚Рѕ РІС‹С…РѕРґ (РґР»СЏ РґР°С‚С‡РёРєР°), РєРѕРіРґР° 0 - РІС…РѕРґ РґР»СЏ РґР°С‚С‡РёРєР°
+logic dht11_data_internal; // РєРѕРіРґР° dht11_data_output=1, РјРѕР¶РЅРѕ РѕС‚РїСЂР°РІРёС‚Р± 1/0 РјРёРєСЂРѕРєРѕРЅС‚СЂРѕР»Р»РµСЂСѓ
+logic [39:0] data_sequence;
+assign dht11_data = dht11_data_output ? dht11_data_internal : 'bz; // tri-state 
     
+    logic        dht11_start;           //signal for initializing DHT11 measurements
+    logic [15:0] dht11_data_rec;            //received data from DHT11
+    logic        dht11_data_available;  //is data from DHT11 available?
+    
+    logic        dht11_data_i;
+    logic        dht11_data_o;
+    logic        dht11_data_o_en;
+
+    assign dht11_data_i = dht11_data;
+    assign dht11_data   = dht11_data_o_en ? dht11_data_o : 'bz;
 
     // Подключаем модуль
-    DHT11 uut (
-        .clk(clk),
-        .rst_n(rst_n),
-        .uart_rx(uart_rx),
-        .uart_tx(uart_tx),
-        .dht11_data(dht11_data),
-        .ready(ready)
-    );
+    DHT11 i_dht11 (
+    // Common
+    .clk             (clk                 ),
+    .rst_n           (rst_n               ),
+    // DHT11 <-> CROSSBAR
+    .uart_rx         (dht11_start         ),
+    .uart_tx         (dht11_data_rec      ),
+    .ready           (dht11_data_available),
+    // External interface
+    .dht11_data_i    (dht11_data_i        ),
+    .dht11_data_o    (dht11_data_o        ),
+    .dht11_data_o_en (dht11_data_o_en     )
+);
     
-    
+
     // Используем tri для inout
-   
-    assign dht11_data = dht11_data_output ? dht11_data_internal : 'bz; // tri-state 
+    //assign dht11_data_i = dht11_data;
+    //assign dht11_data = dht11_data_output ? dht11_data_internal : 'bz; // tri-state 
     //Когда dht11_data_output = 0 - высокий импеданс
     //Когда dht11_data_output = 1 - можем передать данные микроконтроллеру, записанные в dht11_data_internal
 
-    // Сигнал сброса
-        initial begin // выполняется единожды
+
+    initial begin // сброс
         rst_n = 0;
         #1000;
         rst_n = 1;
-        end
+    end
         
 
 
-    // Генерация тактового сигнала
-    initial begin // выполняется единожды
+    // ????????? ????????? ???????
+    initial begin // ??????????? ????????
         clk = 0;
-        forever #(CLK_PERIOD / 2) clk = ~clk; // генерация тактового сигнала
+        forever #(CLK_PERIOD / 2) clk = ~clk; // ????????? ????????? ???????
     end
 
-   // имитация работы UART
+   // ???????? ?????? UART
    initial begin
-   uart_rx = 0;
+   dht11_start = 0;
    #1000;
-   uart_rx = 1; // посылаем 1 микроконтроллеру, чтобы получить 16 бит (8 бит температуры и 8 бит влажности)
-   wait(ready == 1); // ожидаем сигнал готовности, uart_tx выводится в консоль
-   uart_rx = 0;
+   dht11_start = 1; // ???????? 1 ????????????????, ????? ???????? 16 ??? (8 ??? ??????????? ? 8 ??? ?????????)
+   #100;
+   dht11_start = 0;
+   wait(dht11_data_available == 1); // ??????? ?????? ??????????, uart_tx ????????? ? ???????
+   #10000;
+   dht11_start = 1;
    #100_000 $finish;
    end
+
 
     // Имитация работы датчика DHT11
     initial begin
@@ -80,7 +104,7 @@ module DHT11_TB;
          #80_000; // Высокий сигнал (80 мкс)
 
           // Будем передавать такую последовательность бит: 00110101_00000000_00011000_00000000_01001101
-          data_sequence = 40'b0011010100000000000110000000000001001101;
+          data_sequence = 40'b10110010_00000000_00011000_00000000_10101100;
             for (int i = 0; i < 40; i++) begin
                 if (data_sequence[i] == 0) begin
                     // Передаем бит '0'
@@ -108,7 +132,7 @@ module DHT11_TB;
     // Отображение значений в процессе симуляции
     initial begin
         //$monitor(" uart_tx: %b | dht11_data: %b | data_sequence: %b | data_buffer: %b | data_counter: %d | counter_2: %d", uart_tx, dht11_data, data_sequence, uut.data_buffer, uut.data_counter, uut.counter_2);
-        $monitor(" uart_tx: %b | dht11_data: %b | data_sequence: %b | data_buffer: %b | data_counter: %d | a: %d | uart_tx: %b", uart_tx, dht11_data, data_sequence, uut.data_buffer, uut.data_counter, uut.a, uart_tx);
+        $monitor(" uart_rx: %b | dht11_data: %b | data_sequence: %b | data_buffer: %b | data_counter: %d | a: %d | uart_tx: %b | humidity_integer: %b | humidity_real: %b | temperature_integer: %b | temperature_real: %b | checksum: %b", dht11_start, dht11_data, data_sequence, i_dht11.data_buffer, i_dht11.data_counter, i_dht11.a, dht11_data_rec, i_dht11.humidity_integer, i_dht11.humidity_real, i_dht11.temperature_integer, i_dht11.temperature_real,  i_dht11.checksum);
         //$monitor(" uart_tx: %b | dht11_data: %b | data_sequence: %b | counter: %d", uart_tx, dht11_data, data_sequence, uut.counter);
     end
 endmodule
